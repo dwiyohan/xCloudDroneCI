e8e57ac3d5e0205ae049cd9d7565a6e9aae6ec7e
Bizart <80744021+XBIZART@users.noreply.github.com>
2021-09-30 22:39:49 +0700

    Update kernel_build.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -18,7 +18,7 @@
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 $KERNEL_SOURCE $KERNEL_BRANCH $DEVICE_CODENAME
-git clone --depth=1 https://github.com/cbendot/elastics-toolchain clang
+git clone --depth=1 https://github.com/XBIZART/GengKapak-clang clang
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
